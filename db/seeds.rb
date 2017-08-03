a30f8dda53c65c26f8dce41f452553cb593ecaba
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-03 11:54:40 +0200

    added routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  rand_ingredients = Ingredient.create(name: Faker::Food.ingredient)
+  rand_ingredients.save!
+end
+
