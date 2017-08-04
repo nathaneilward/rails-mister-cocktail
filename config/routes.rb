423b01916c5550f604b6aad23c3205507f9e0769
Nathaneil Ward <nathaneilw@gmail.com>
2017-08-04 15:12:47 +0200

    started front end

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :cocktails, only: [ :index, :show, :create, :new ] do
+  resources :cocktails, only: [ :index, :destroy, :show, :create, :new, ] do
     resources :doses, only: [ :create, :new ]
   end
   resources :doses, only: [ :destroy ]
